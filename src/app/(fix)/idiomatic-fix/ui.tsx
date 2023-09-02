51b4d4e9846195a807af3aa02da5a038f74d3b89
Leo <omarleopaz@hotmail.com>
2023-09-02 00:44:49 +0000

    breaking change

--- src/app/(fix)/idiomatic-fix/ui.tsx
+++ src/app/(fix)/idiomatic-fix/ui.tsx
@@ -1,7 +1,15 @@
-import { UserProfile } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton, UserProfile } from "@clerk/nextjs";
 
 const Ui = () => {
-  return <UserProfile />;
+  return <>
+    {/* <SignedIn>
+      <UserButton />
+    </SignedIn>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut> */}
+    <UserProfile />
+  </>;
 };
 
 export default Ui;
